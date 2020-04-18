8446466f85ac676372959e8a4212694853564bd2
Sagar Jogadia <sagarjogadia28@gmail.com>
2020-04-18 16:54:31 -0400

    Add click event for add button

---
+++ main.js
@@ -0,0 +1,39 @@
+/************************* SELECTORS *************************/
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+/************************* EVENT LISTENERS *************************/
+todoButton.addEventListener('click', addTodo);
+
+/************************* FUNCTIONS *************************/
+function addTodo(event) {
+    //Prevent the form from submitting
+    event.preventDefault();
+
+    //Create a To-do div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    //Create a li item
+    const newTodo = document.createElement('li');
+    newTodo.innerText = "Hello";
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //Create check button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    //Create trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+
+    //Add div to lists
+    todoList.appendChild(todoDiv);
+
+}
