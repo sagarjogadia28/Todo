8e62f0e98c0e866c288612d4402e8c32aaaf902c
Sagar Jogadia <sagarjogadia28@gmail.com>
2020-04-19 23:59:45 -0400

    Add local storage

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ const filterOption = document.querySelector(".filter-todo");
 
 
 /************************* EVENT LISTENERS *************************/
+document.addEventListener("DOMContentLoaded", getLocalTodos);
 todoButton.addEventListener('click', addTodo);
-todoList.addEventListener('click', deleteCheck);
+todoList.addEventListener('click', deleteAndMarkComplete);
 filterOption.addEventListener('change', filterTodo);
 
 /************************* FUNCTIONS *************************/
@@ -15,15 +16,110 @@ function addTodo(event) {
     //Prevent the form from submitting
     event.preventDefault();
 
+    //Create a to-do item
+    createATodo(todoInput.value, true);
+
+    //Delete the value from the input
+    todoInput.value = "";
+}
+
+function deleteAndMarkComplete(e) {
+    const clickedItem = e.target;
+
+    //Delete the to-do item
+    if (clickedItem.classList[0] === 'trash-btn') {
+        const parentElement = clickedItem.parentElement;
+        parentElement.classList.toggle('fall');
+        parentElement.addEventListener('transitionend', () => {
+            parentElement.remove();
+        });
+        removeLocalTodos(parentElement);
+    }
+
+    //Mark the item as completed
+    if (clickedItem.classList[0] === 'complete-btn') {
+        clickedItem.parentElement.classList.toggle('completed');
+    }
+}
+
+//Filter the to-do list according to completed or not
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function (todo) {
+        switch (e.target.value) {
+            case 'all':
+                todo.style.display = 'flex';
+                break;
+            case 'incomplete':
+                if (todo.classList.contains('completed'))
+                    todo.style.display = 'none';
+                else
+                    todo.style.display = 'flex';
+                break;
+            case 'completed':
+                if (todo.classList.contains('completed'))
+                    todo.style.display = 'flex';
+                else
+                    todo.style.display = 'none';
+
+                break;
+        }
+    });
+}
+
+//Initialize the to-do array if null or get it from local storage
+function getTodoArray() {
+    let todoArray;
+    if (localStorage.getItem('todos') === null) {
+        todoArray = [];
+    } else {
+        todoArray = JSON.parse(localStorage.getItem('todos'));
+    }
+    return todoArray;
+}
+
+//Save each to-do to local storage
+function saveLocalTodos(todo) {
+    let todoArray = getTodoArray();
+    todoArray.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todoArray));
+}
+
+//Get to-do list from local storage when page is reloaded
+function getLocalTodos() {
+    let todoArray = getTodoArray();
+    todoArray.forEach(function (todo) {
+        createATodo(todo, false);
+    });
+}
+
+//Remove to-do item from local storage which clicked on trash button
+function removeLocalTodos(todo) {
+    let todoArray = getTodoArray();
+    const position = todoArray.indexOf(todo.children[0].innerText);
+    todoArray.splice(position, 1);
+    localStorage.setItem('todos', JSON.stringify(todoArray));
+}
+
+//Create a to-do item with parameter: value to set and whether to save to local storage
+function createATodo(value, saveToLocal) {
     //Create a To-do div
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
 
+    //If nothing entered by user then return
+    if (value === '')
+        return;
+
     //Create a li item
     const newTodo = document.createElement('li');
-    newTodo.innerText = todoInput.value;
+    newTodo.innerText = value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
+
+    //Add to-do to local storage
+    if (saveToLocal)
+        saveLocalTodos(todoInput.value);
 
     //Create check button
     const completedButton = document.createElement('button');
@@ -39,52 +135,4 @@ function addTodo(event) {
 
     //Add div to lists
     todoList.appendChild(todoDiv);
-
-    //Delete the value from the input
-    todoInput.value = "";
-}
-
-function deleteCheck(e) {
-    const clickedItem = e.target;
-
-    //Delete the to-do item
-    if (clickedItem.classList[0] === 'trash-btn') {
-        const parentElement = clickedItem.parentElement;
-        parentElement.classList.toggle('fall');
-        parentElement.addEventListener('transitionend', () => {
-            parentElement.remove();
-        });
-
-    }
-
-    //Check the item
-    if (clickedItem.classList[0] === 'complete-btn') {
-        clickedItem.parentElement.classList.toggle('completed');
-    }
-
-}
-
-function filterTodo(e) {
-    const todos = todoList.childNodes;
-    todos.forEach(function (todo) {
-        switch (e.target.value) {
-            case 'all':
-                todo.style.display = 'flex';
-                break;
-            case 'incomplete':
-                if (todo.classList.contains('completed')) {
-                    todo.style.display = 'none';
-                } else {
-                    todo.style.display = 'flex';
-                }
-                break;
-            case 'completed':
-                if (todo.classList.contains('completed')) {
-                    todo.style.display = 'flex';
-                } else {
-                    todo.style.display = 'none';
-                }
-                break;
-        }
-    });
 }
